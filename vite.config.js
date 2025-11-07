c6db336e2ac015c385cdd0bd223329a586b0b353
adityadevraj699 <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2025-11-07 20:47:09 +0530

    feat: Add Vercel configuration and set build output directory in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   base: '/',   // 👈 Keep this (important for Vercel)
+  build: {
+    outDir: 'dist'
+  }
 })
